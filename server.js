5029dd1beeb4b4651c408fb2322dad4f73a343a4
madhanrajR <madhanit31@gmail.com>
2020-01-08 10:42:39 +0530

    chatapp

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular1-on1-chat'));
+app.use(express.static(__dirname + '/dist/Angular1On1Chat'));
 
 app.get('/*', function(req,res) {
  res.setMaxListeners(0);   
-res.sendFile(path.join(__dirname+'/dist/angular1-on1-chat/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Angular1On1Chat/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
